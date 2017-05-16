6dff14c7d74360717357995657d7e3a2a956c6b0
Thomas Rich <trich1230@gmail.com>
2017-05-16 12:59:21 -0700

    small

---
+++ src/DataTable/utils/queryParams.js
@@ -0,0 +1,235 @@
+import { reset } from "redux-form";
+import isEqual from "lodash/isEqual";
+import queryString from "query-string";
+
+export default function queryParams({ columns, schema, defaults = {} }) {
+  var defaultParams = {
+    pageSize: 10,
+    order: "",
+    where: {},
+    include: [],
+    searchTerm: undefined, //undefined helps us compare when this has been changed to an empty string
+    page: 1,
+    ...defaults
+  };
+
+  function getCurrentParams(location) {
+    const { search } = location;
+    let currentParams = {
+      ...defaultParams,
+      ...jsonParseNested(queryString.parse(search))
+    };
+    return currentParams;
+  }
+
+  return {
+    getQueryParamsFromRouter: function({ location }) {
+      let graphqlQueryParams = getCurrentParams(location);
+      const { page, pageSize } = graphqlQueryParams;
+      //convert params from user readable to what our api expects
+      // aka page -> offset & pageSize -> limit
+      graphqlQueryParams.offset = (page - 1) * pageSize;
+      graphqlQueryParams.limit = pageSize;
+
+      delete graphqlQueryParams.pageSize;
+      delete graphqlQueryParams.page;
+
+      const {
+        searchTerm,
+        selectedFilter,
+        filterValue,
+        fieldName
+      } = graphqlQueryParams;
+      delete graphqlQueryParams.searchTerm;
+      delete graphqlQueryParams.selectedFilter;
+      delete graphqlQueryParams.filterValue;
+      delete graphqlQueryParams.fieldName;
+      if (selectedFilter) {
+        const subFilter = getSubFilter(selectedFilter, filterValue, fieldName);
+        const { path, model } = schema[fieldName];
+        graphqlQueryParams = {
+          ...graphqlQueryParams,
+          where: {
+            [model ? `$${path}$` : fieldName]: subFilter
+          },
+          include: model
+            ? {
+                [model]: {
+                  model,
+                  required: false
+                }
+              }
+            : undefined
+        };
+      }
+      if (searchTerm) {
+        //custom logic based on the table schema to get the sequelize query
+        let include = {};
+        let or = {};
+        columns.forEach(function(column) {
+          const { model, type, path } = schema.fields[column];
+          if (type === "string" || type === "lookup") {
+            var likeObj = { iLike: "%" + graphqlQueryParams.searchTerm + "%" };
+            if (model) {
+              const includeObj = include[model] || {
+                model,
+                required: false
+              };
+              include[model] = includeObj;
+              or["$" + path + "$"] = likeObj;
+            } else {
+              or[column] = likeObj;
+            }
+          }
+        }, {});
+        graphqlQueryParams = {
+          ...graphqlQueryParams,
+          offset: 0,
+          where: {
+            $or: or
+          },
+          include
+        };
+      }
+      return {
+        queryParams: graphqlQueryParams,
+        page,
+        pageSize,
+        order: graphqlQueryParams.order,
+        selectedFilter,
+        filterValue,
+        fieldName,
+        searchTerm
+      };
+    },
+    setQueryParamsOnRouter: function(dispatch, { history, location }) {
+      function setSearchTerm(searchTerm) {
+        const currentParams = getCurrentParams(location);
+        let newParams = {
+          ...currentParams,
+          searchTerm
+        };
+        history.push({
+          search: `?${queryString.stringify(jsonStringifyNested(newParams))}`
+        });
+      }
+      function setFilter({ selectedFilter, filterValue, fieldName }) {
+        const currentParams = getCurrentParams(location);
+
+        let newParams = {
+          ...currentParams,
+          selectedFilter,
+          fieldName,
+          filterValue,
+          searchTerm: undefined
+        };
+        history.push({
+          search: `?${queryString.stringify(jsonStringifyNested(newParams))}`
+        });
+        setTimeout(function() {
+          dispatch(reset("dataTableSearchInput"));
+        });
+      }
+      function clearFilters() {
+        history.push({
+          search: ""
+        });
+      }
+      function setPageSize(pageSize) {
+        const currentParams = getCurrentParams(location);
+        let newParams = {
+          ...currentParams,
+          pageSize
+        };
+        history.push({
+          search: `?${queryString.stringify(jsonStringifyNested(newParams))}`
+        });
+      }
+      function setOrder(order) {
+        const currentParams = getCurrentParams(location);
+        let newParams = {
+          ...currentParams,
+          order
+        };
+        history.push({
+          search: `?${queryString.stringify(jsonStringifyNested(newParams))}`
+        });
+      }
+      function setPage(page) {
+        const currentParams = getCurrentParams(location);
+        let newParams = {
+          ...currentParams,
+          page
+        };
+        history.push({
+          search: `?${queryString.stringify(jsonStringifyNested(newParams))}`
+        });
+      }
+      return {
+        setSearchTerm,
+        setFilter,
+        clearFilters,
+        setPageSize,
+        setPage,
+        setOrder
+      };
+    }
+  };
+}
+
+function jsonStringifyNested(obj) {
+  var newObj = {};
+  Object.keys(obj).forEach(function(key) {
+    var val = obj[key];
+    if (val !== null && typeof val === "object") {
+      newObj[key] = JSON.stringify(val);
+    } else {
+      newObj[key] = val;
+    }
+  });
+  return newObj;
+}
+
+function jsonParseNested(obj) {
+  var newObj = {};
+  Object.keys(obj).forEach(function(key) {
+    var val = obj[key];
+    try {
+      newObj[key] = JSON.parse(val);
+    } catch (e) {
+      newObj[key] = val;
+    }
+  });
+  return newObj;
+}
+
+function getSubFilter(selectedFilter, filterValue, fieldName) {
+  if (selectedFilter === "Text starts with")
+    return { $iLike: `${filterValue}%` };
+  else if (selectedFilter === "Text ends with")
+    return { $iLike: `%${filterValue}` };
+  else if (selectedFilter === "Text contains")
+    return { $iLike: `%${filterValue}%` };
+  else if (selectedFilter === "Text is exactly")
+    return { $iLike: `${filterValue}` };
+  else if (selectedFilter === "Date is between")
+    // else if (selectedFilter === "Date is")
+    //   return {
+    //     $between: [
+    //       filterValue.getTime(),
+    //       filterValue.getTime() + 4320000
+    //     ]
+    //   };
+    return {
+      $between: [filterValue[0].getTime(), filterValue[1].getTime()]
+    };
+  else if (selectedFilter === "Date is before")
+    return { $lt: filterValue.getTime() };
+  else if (selectedFilter === "Date is after")
+    return { $gt: filterValue.getTime() };
+  else if (selectedFilter === "Greater than") return { $gt: filterValue };
+  else if (selectedFilter === "Less than") return { $lt: filterValue };
+  else if (selectedFilter === "In range")
+    return { $between: [filterValue[0], filterValue[1]] };
+  else if (selectedFilter === "Equal to") return filterValue;
+}
---
+++ src/DataTable/utils/routeDoubleClick.js
@@ -0,0 +1,8 @@
+export default function routeDoubleClick (row, rowIndex, history) {
+  const recordType = row["__typename"];
+  const recordId = row["dbId"];
+  const route = "/" + recordType + "s" + "/" + recordId;
+  history
+    ? history.push(route)
+    : console.warn("router history not passed to datatable");
+};
