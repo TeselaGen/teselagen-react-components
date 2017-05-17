3f2a9dd960bd1df68a4009952b08c2dcf7dbfb54
Thomas Rich <trich1230@gmail.com>
2017-05-17 10:49:50 -0700

    adding bottom toastr ability

--- src/toastr.js
+++ src/toastr.js
@@ -5,34 +5,47 @@ const TopToaster = Toaster.create({
   position: Position.TOP
 });
 
+const BottomToaster = Toaster.create({
+  className: "bottom-toaster",
+  position: Position.BOTTOM
+});
+
 if (!window.toastr) window.toastr = {};
 if (!window.toastr.success)
-  window.toastr.success = function(message) {
-    TopToaster.show({
+  window.toastr.success = function(message, options) {
+    options = options || {};
+    var toastToUse = options.bottom ? BottomToaster : TopToaster;
+    toastToUse.show({
       intent: Intent.SUCCESS,
       message
     });
   };
 
 if (!window.toastr.error)
-  window.toastr.error = function(message) {
-    TopToaster.show({
+  window.toastr.error = function(message, options) {
+    options = options || {};
+    var toastToUse = options.bottom ? BottomToaster : TopToaster;
+    toastToUse.show({
       intent: Intent.DANGER,
       message
     });
   };
 
 if (!window.toastr.warning)
-  window.toastr.warning = function(message) {
-    TopToaster.show({
+  window.toastr.warning = function(message, options) {
+    options = options || {};
+    var toastToUse = options.bottom ? BottomToaster : TopToaster;
+    toastToUse.show({
       intent: Intent.WARNING,
       message
     });
   };
 
 if (!window.toastr.info)
-  window.toastr.info = function(message) {
-    TopToaster.show({
+  window.toastr.info = function(message, options) {
+    options = options || {};
+    var toastToUse = options.bottom ? BottomToaster : TopToaster;
+    toastToUse.show({
       message
     });
   };
