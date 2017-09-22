de56490473bcdda35441fbcb44df8dfc224d878a
Rodrigo Pavez <rpavez@gmail.com>
2017-09-22 14:21:18 -0300

    test

--- demo/src/index.js
+++ demo/src/index.js
@@ -92,7 +92,7 @@ function HomePage() {
               style={{ color: 'inherit', textDecoration: 'none' }}
             >
               <div style={  {borderLeft: index !== 0 && '2px solid grey'}} className={'pt-popover-dismiss pt-menu-item'}>
-                {name} demo
+                {name} demo2
               </div>
 
             </Link>
