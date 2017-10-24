fe514c9250150deb796c20b1102b014ae96ed8a7
Taoh Green <greenmachine777@gmail.com>
2017-10-24 11:40:02 -0700

    new loading component

---
+++ src/DNALoader/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+export default function DNALoader() {
+  return (
+    <div className="DNA_cont">
+      <div className="nucleobase" />
+      <div className="nucleobase" />
+      <div className="nucleobase" />
+      <div className="nucleobase" />
+      <div className="nucleobase" />
+      <div className="nucleobase" />
+      <div className="nucleobase" />
+      <div className="nucleobase" />
+      <div className="nucleobase" />
+      <div className="nucleobase" />
+    </div>
+  );
+}
--- src/enhancers/withQuery/index.js
+++ src/enhancers/withQuery/index.js
@@ -3,6 +3,5 @@ import Loading from "../../Loading";
 
 export default function withQuery(fragment, options = {}) {
   //passing a default LoadingComponent to withQuery
-  //we need to be able to use withQuery on the backend so can't have the loading component importing .css files
   return _withQuery(fragment, { ...options, LoadingComp: Loading });
 }
