71bd32ecf28da0158daed5d82692b3783b44dd3c
Taoh Green <greenmachine777@gmail.com>
2017-11-01 17:25:47 -0700

    update route double click for kebab case routes

--- src/DataTable/utils/routeDoubleClick.js
+++ src/DataTable/utils/routeDoubleClick.js
@@ -1,8 +1,10 @@
 import pluralize from "pluralize";
+import { kebabCase } from "lodash";
+
 export default function routeDoubleClick(row, rowIndex, history) {
-  const recordType = row["__typename"];
+  const recordType = row["__typename"] || "";
   const recordId = row["dbId"] || row["id"];
-  const route = "/" + pluralize(recordType) + "/" + recordId;
+  const route = "/" + pluralize(kebabCase(recordType)) + "/" + recordId;
   history
     ? history.push(route)
     : console.warn("react router history not passed to datatable");
