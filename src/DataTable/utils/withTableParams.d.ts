2b2c3a43875ea16770f9699a9896a93275f93136
Taoh Green <greenmachine777@gmail.com>
2017-11-14 14:37:55 -0800

    add withTableParams additionalFilter prop typings

--- src/DataTable/utils/withTableParams.d.ts
+++ src/DataTable/utils/withTableParams.d.ts
@@ -9,9 +9,10 @@ export interface IDefaults {
 }
 
 export default function withTableParams({
-  formName: String; // - required unique identifier for the table
-  schema: Boolean; //  - The data table schema
-  urlConnected: Boolean; //  - whether the table should connect to/update the URL
-  withSelectedEntities: Boolean; //  - whether or not to pass the selected entities
-  defaults: defaults; // - tableParam defaults such as pageSize, filter, etc
+  formName: String, // - required unique identifier for the table
+  schema: Boolean, //  - The data table schema
+  urlConnected: Boolean, //  - whether the table should connect to/update the URL
+  withSelectedEntities: Boolean, //  - whether or not to pass the selected entities
+  defaults: defaults, // - tableParam defaults such as pageSize, filter, etc
+  additionalFilter:  object | function // - pass an additional filter as an object or function (args - props, qb) which will be added to the filters for the table
 }) {}
