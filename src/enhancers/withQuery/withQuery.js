501e58494005a94b27dfba5c28308abc5eac4c00
Taoh Green <greenmachine777@gmail.com>
2017-11-15 14:07:11 -0800

    minHeight on withQuery LoadingComp

--- src/enhancers/withQuery/withQuery.js
+++ src/enhancers/withQuery/withQuery.js
@@ -217,13 +217,11 @@ export default function withQuery(fragment, options = {}) {
             !deepEqual(nextProps.data.error, this.props.data.error)
           ) {
             const error = nextProps.data.error;
-            if(this.props.loggedIn)
-            {
+            if (this.props.loggedIn) {
               console.error("error:", error);
               window.toastr.error(`Error loading ${queryNameToUse}`);
-            }
-            else {
-              console.log("Error supressed, not logged in")
+            } else {
+              console.log("Error supressed, not logged in");
             }
           }
         }
@@ -231,7 +229,7 @@ export default function withQuery(fragment, options = {}) {
           const { data = {} } = this.props;
           const { loading } = data;
           if (loading && showLoading) {
-            return <LoadingComp loading />;
+            return <LoadingComp style={{ minHeight: 200 }} />;
           }
           return <WrappedComponent {...this.props} />;
         }
