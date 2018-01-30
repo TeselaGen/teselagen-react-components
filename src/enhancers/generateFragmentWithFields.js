508cff3ab69fe60cfead24b7d3f62f944ad02611
Thomas Rich <tnrich@teselagen.com>
2018-01-30 15:22:50 -0800

    updating generateFragmentWithFields to support fragments

--- src/enhancers/generateFragmentWithFields.js
+++ src/enhancers/generateFragmentWithFields.js
@@ -1,9 +1,13 @@
 import { uniqueId } from "lodash";
-import gql from "graphql-tag";
+import { gql } from "react-apollo";
 
 /* eslint graphql/template-strings:0 */
-export default (model, fields) => gql`
+export default (model, fields, fragments = []) =>
+  gql`
   fragment __${model}FragmentGenerated${uniqueId()} on ${model} {
     ${Array.isArray(fields) ? fields.join("\n") : fields}
   }
-`;
+  ${Array.isArray(fragments)
+    ? fragments.map(f => f.loc.source.body).join("\n")
+    : fragments}
+  `;
