92768fa63e311d4be88dd563f9a7ad489daf3707
Taoh Green <taohgreen@gmail.com>
2018-04-18 14:12:48 -0700

    Added width 100% to loading

--- src/Loading/index.js
+++ src/Loading/index.js
@@ -38,11 +38,14 @@ export default class Loading extends React.Component {
 
     if (loading || !children) {
       if (!longerThan200MS && !bounce && !inDialog) {
-        return <div className={"tg-flex justify-center align-center"} />;
+        return <div />;
       }
       return (
         <div
           className={"tg-loader-container tg-flex justify-center align-center"}
+          style={{
+            width: "100%"
+          }}
         >
           <LoaderComp style={style} className={className} />
         </div>
