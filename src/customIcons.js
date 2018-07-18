4623d940a3a17a8a9479986dceff9a923a0091df
Cesar Pinto <cesar.pinto@teselagen.com>
2018-07-18 00:15:42 -0700

    added keyboardIcon to Custom icons

--- src/customIcons.js
+++ src/customIcons.js
@@ -196,12 +196,9 @@ export const moleculeIcon = ptIconWrapper(
   />,
   408
 );
-<<<<<<< Updated upstream
-=======
 
 export const keyboardIcon = ptIconWrapper(
   <path
-    fill="#5c7080"
     d="M459,76.5H51c-28.05,0-51,22.95-51,51v255c0,28.05,22.95,51,51,51h408c28.05,0,51-22.95,51-51v-255
 			C510,99.45,487.05,76.5,459,76.5z M229.5,153h51v51h-51V153z M229.5,229.5h51v51h-51V229.5z M153,153h51v51h-51V153z M153,229.5
 			h51v51h-51V229.5z M127.5,280.5h-51v-51h51V280.5z M127.5,204h-51v-51h51V204z M357,382.5H153v-51h204V382.5z M357,280.5h-51v-51
@@ -209,4 +206,3 @@ export const keyboardIcon = ptIconWrapper(
   />,
   510
 );
->>>>>>> Stashed changes
