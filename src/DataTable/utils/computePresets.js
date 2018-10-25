6fe576c6af567f842f2457a3feffda10a24a3461
Taoh Green <greenmachine777@gmail.com>
2018-10-25 16:10:27 -0700

    Merge in compute presets to override undefined values

--- src/DataTable/utils/computePresets.js
+++ src/DataTable/utils/computePresets.js
@@ -1,38 +1,43 @@
+import { merge } from "lodash";
 //we use this to make adding preset prop groups simpler
 export default function computePresets(props) {
   const { isSimple } = props;
   let toReturn = { ...props };
   if (isSimple) {
     //isSimplePreset
-    toReturn = {
-      noHeader: true,
-      noFooter: !props.withPaging,
-      noPadding: true,
-      noFullscreenButton: true,
-      hidePageSizeWhenPossible: true,
-      isInfinite: !props.withPaging,
-      hideSelectedCount: true,
-      withTitle: false,
-      withSearch: false,
-      withPaging: false,
-      withFilter: false,
-      ...toReturn
-    };
+    toReturn = merge(
+      {
+        noHeader: true,
+        noFooter: !props.withPaging,
+        noPadding: true,
+        noFullscreenButton: true,
+        hidePageSizeWhenPossible: true,
+        isInfinite: !props.withPaging,
+        hideSelectedCount: true,
+        withTitle: false,
+        withSearch: false,
+        withPaging: false,
+        withFilter: false
+      },
+      toReturn
+    );
   } else {
-    toReturn = {
-      // the usual defaults:
-      noFooter: false,
-      noPadding: false,
-      noFullscreenButton: false,
-      hidePageSizeWhenPossible: false,
-      isInfinite: false,
-      hideSelectedCount: false,
-      withTitle: true,
-      withSearch: true,
-      withPaging: true,
-      withFilter: true,
-      ...toReturn
-    };
+    toReturn = merge(
+      {
+        // the usual defaults:
+        noFooter: false,
+        noPadding: false,
+        noFullscreenButton: false,
+        hidePageSizeWhenPossible: false,
+        isInfinite: false,
+        hideSelectedCount: false,
+        withTitle: true,
+        withSearch: true,
+        withPaging: true,
+        withFilter: true
+      },
+      toReturn
+    );
   }
   return toReturn;
 }
