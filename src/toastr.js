abc69748b4f4b38d6422211c6f98a98e0e821d17
Taoh Green <greenmachine777@gmail.com>
2018-11-01 12:16:26 -0700

    Updated toast api to allow updates

--- src/toastr.js
+++ src/toastr.js
@@ -14,29 +14,39 @@ const generateToast = intent => (message, options) => {
   options = options || {};
   const toastToUse = options.bottom ? BottomToaster : TopToaster;
 
-  const uniqKey = toastToUse.show({
-    intent,
-    message,
-    action: options.action,
-    icon: options.icon
-  });
-  return function clear() {
+  const uniqKey = toastToUse.show(
+    {
+      intent,
+      message,
+      timeout: options.timeout,
+      action: options.action,
+      icon: options.icon
+    },
+    options.key
+  );
+  function clear() {
     toastToUse.dismiss(uniqKey);
-  };
+  }
+  clear.key = uniqKey;
+  return clear;
 };
 
 function preventDuplicates(func) {
   const previousToasts = {};
   return (message, options = {}) => {
-    if (previousToasts[message]) {
-      previousToasts[message](); //clear it!
-    }
-
-    setTimeout(() => {
-      delete previousToasts[message];
-    }, options.timeout || 5000);
     const clearToast = func(message, options);
-    previousToasts[message] = clearToast;
+    // no duplicate check for toasts with updates
+    if (!options.key) {
+      if (!options.key && previousToasts[message]) {
+        previousToasts[message](); //clear it!
+      }
+
+      setTimeout(() => {
+        delete previousToasts[message];
+      }, options.timeout || 5000);
+
+      previousToasts[message] = clearToast;
+    }
     return clearToast;
   };
 }
