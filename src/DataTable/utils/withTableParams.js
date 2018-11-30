15a1a01de9f30f4bbc251c9699472fd55f592c52
Taoh <tgreen@teselagen.com>
2018-11-29 16:38:15 -0800

    Pass is local call

--- src/DataTable/utils/withTableParams.js
+++ src/DataTable/utils/withTableParams.js
@@ -138,6 +138,7 @@ export default function withTableParams(compOrOpts, pTopLevelOpts) {
       noOrderError,
       withDisplayOptions,
       cellRenderer,
+      isLocalCall,
       model,
       schema,
       ...getQueryParams({
