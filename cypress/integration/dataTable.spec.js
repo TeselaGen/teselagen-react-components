0e3382a7ba9f8f53070b9a9715c6f3d1c82eafb9
jonjian <jonjian@gmail.com>
2019-01-15 13:04:14 -0800

    Added tests

--- cypress/integration/dataTable.spec.js
+++ cypress/integration/dataTable.spec.js
@@ -2,34 +2,35 @@ describe("formComponents", () => {
   beforeEach(() => {
     cy.visit("#/DataTable");
   });
-  it(`it can copy a single row, selected rows, or cells to the clipboard`, () => {
+  it.only(`it can copy a single row, selected rows, or cells to the clipboard`, () => {
     //  - copying a single row (selected or not)
-    // cy.get(`[data-test="tgCell_type.special"]`)
-    //   .first()
-    //   .click();
-    // cy.get(`[data-test="tgCell_type.special"]`)
-    //   .first()
-    //   .trigger("contextmenu");
-    // cy.contains("Copy Row to Clipboard").click();
-    // // - copying a single cell
-    // cy.get(`[data-test="tgCell_type.special"]`)
-    //   .first()
-    //   .click();
-    // cy.get(`[data-test="tgCell_type.special"]`)
-    //   .first()
-    //   .trigger("contextmenu");
-    // cy.contains("Copy Cell to Clipboard").click();
-    // // - copying multiple selected rows
-    // cy.get(`[data-test="tgCell_type.special"]`)
-    //   .first()
-    //   .click();
-    // cy.get(`[data-test="tgCell_type.special"]`)
-    //   .eq(1)
-    //   .click();
-    // cy.get(`[data-test="tgCell_type.special"]`)
-    //   .first()
-    //   .trigger("contextmenu");
-    // cy.contains("Copy Selected Rows to Clipboard").click();
+    cy.get(`[data-test="tgCell_type.special"]`)
+      .first()
+      .click();
+    cy.get(`[data-test="tgCell_type.special"]`)
+      .first()
+      .trigger("contextmenu");
+    cy.contains("Copy Row to Clipboard").click();
+    // - copying a single cell
+    cy.get(`[data-test="tgCell_type.special"]`)
+      .first()
+      .click();
+    cy.get(`[data-test="tgCell_type.special"]`)
+      .first()
+      .trigger("contextmenu");
+    cy.contains("Copy Cell to Clipboard").click();
+    // - copying multiple selected rows
+    cy.get(`[data-test="tgCell_type.special"]`)
+      .first()
+      .click();
+    cy.get(`[data-test="tgCell_type.special"]`)
+      .eq(1)
+      .click();
+    cy.get(`[data-test="tgCell_type.special"]`)
+      .first()
+      .trigger("contextmenu");
+    cy.contains("Copy Selected Rows to Clipboard").click();
+
     // make sure certain columns that shouldn't be copyable are not copied (different than copyable columns that are simply empty)
     // cy.contains("Copy Selected Rows to Clipboard").click()
   });
