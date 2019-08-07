1afc19fe75290387d652de0797f4ac73a10c495d
tnrich <tnrich@teselagen.com>
2019-08-06 17:59:01 -0700

    fixing tests, adding a helper util showDialogOnDocBody

---
+++ src/showDialogOnDocBody.js
@@ -0,0 +1,21 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import uniqid from "uniqid";
+
+//this is only really useful for unconnected standalone simple dialogs
+//remember to pass usePortal={false} to the <Dialog/> component so it will close properly
+export default function showDialogOnDocBody(DialogComp) {
+  const dialogHolder = document.createElement("div");
+  const className = "myDialog" + uniqid();
+  dialogHolder.className = className;
+  document.body.appendChild(dialogHolder);
+
+  ReactDOM.render(
+    <DialogComp
+      hideModal={() => {
+        document.querySelector("." + className).remove();
+      }}
+    />,
+    dialogHolder
+  );
+}
