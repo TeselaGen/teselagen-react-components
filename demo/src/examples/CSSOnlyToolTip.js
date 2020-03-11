10269e4cf1ea48c9711263c1ff089d1b56a0556a
Thomas <trich1230@gmail.com>
2020-03-11 09:43:49 -0700

    adding css-only tooltip from balloon.css

---
+++ demo/src/examples/CSSOnlyToolTip.js
@@ -0,0 +1,12 @@
+function CustomIconsDemo() {
+  return (
+    <div>
+      <h3>See https://kazzkiq.github.io/balloon.css/ for more examples</h3>
+
+      <button aria-label="Whats up!" data-balloon-pos="up">Hover me!</button>
+      <button aria-label="Whats up!" data-balloon-pos="down">Hover me!</button>
+    </div>
+  );
+}
+
+render(CustomIconsDemo);
