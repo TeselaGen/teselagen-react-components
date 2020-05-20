c751566833a684b8885844aa28c68abb94bf4e68
Thomas <trich1230@gmail.com>
2020-05-20 16:32:28 -0700

    updating ResizableDraggableDialog to support Rnd v10 and adding snapping to bounds on window resize

--- demo/src/examples/ResizableDraggableDialog.js
+++ demo/src/examples/ResizableDraggableDialog.js
@@ -1,39 +1,43 @@
 class Example extends Component {
   constructor(props) {
     super(props);
-    this.handleChange = this.handleChange.bind(this);
     this.state = {
       isOpen: true
     };
   }
 
-  handleChange() {
-    this.setState({ isOpen: !this.state.isOpen });
-  }
   render() {
     return (
       <div>
-        <Button onClick={this.handleChange}> Open Resizable Dialog</Button>
+        <Button
+          onClick={() => {
+            this.setState({ isOpen: true });
+          }}
+        >
+          Open Resizable Dialog
+        </Button>
         <br />
         <br />
         <ResizableDraggableDialog
           isOpen={this.state.isOpen}
-          onClose={this.handleChange}
+          onClose={() => {
+            this.setState({ isOpen: false });
+          }}
           height={300}
           width={300}
           title={"I'm Resizable and Draggable!"}
         >
           <div className={Classes.DIALOG_BODY}>
-      I am a dialog
-      <div style={{width: 250}}>with a bunch of stuff in it</div>
-      {[1, 2, 3, 4, 5, 5, 6, 6, 77, 7, 12, 2, 34].map((num, i) => {
-        return (
-          <div key={i} style={{ height: 40, background: Math.random() }}>
-            {num}
+            I am a dialog
+            <div style={{ width: 250 }}>with a bunch of stuff in it</div>
+            {[1, 2, 3, 4, 5, 5, 6, 6, 77, 7, 12, 2, 34].map((num, i) => {
+              return (
+                <div key={i} style={{ height: 40, background: Math.random() }}>
+                  {num}
+                </div>
+              );
+            })}
           </div>
-        );
-      })}
-    </div>
         </ResizableDraggableDialog>
       </div>
     );
--- src/ResizableDraggableDialog/index.js
+++ src/ResizableDraggableDialog/index.js
@@ -1,21 +1,36 @@
-// import Rnd from "react-rnd";
 import React from "react";
 import { Dialog, Classes } from "@blueprintjs/core";
-import Rnd from "react-rnd";
+import { Rnd } from "react-rnd";
 import "./style.css";
 
 export default class ResizableDraggableDialog extends React.Component {
-  constructor(props) {
-    super(props);
-    const {
-      width: defaultDialogWidth = 400,
-      height: defaultDialogHeight = 450
-    } = this.props;
-    const { windowWidth, windowHeight } = this.getWindowWidthAndHeight();
-    this.state = {
-      x: Math.round(Math.max((windowWidth - defaultDialogWidth) / 2, 0)),
-      y: Math.round(Math.max((windowHeight - defaultDialogHeight) / 2, 0))
-    };
+  componentDidMount() {
+    window.addEventListener("resize", this.onWindowResize);
+  }
+
+  onWindowResize = () => {
+    //trigger a fake drag event here so that the dialog will stay in the window on window resize
+    const targetNode = document.querySelector(`.${Classes.DIALOG_HEADER}`);
+    if (targetNode) {
+      //--- Simulate a natural mouse-click sequence.
+      triggerMouseEvent(targetNode, "mouseover");
+      triggerMouseEvent(targetNode, "mousedown");
+      triggerMouseEvent(document, "mousemove");
+      triggerMouseEvent(targetNode, "mouseup");
+      triggerMouseEvent(targetNode, "click");
+    } else {
+      // eslint-disable-next-line no-console
+      console.log("*** Target node not found!");
+    }
+
+    function triggerMouseEvent(node, eventType) {
+      const clickEvent = document.createEvent("MouseEvents");
+      clickEvent.initEvent(eventType, true, true);
+      node.dispatchEvent(clickEvent);
+    }
+  };
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.onWindowResize);
   }
 
   getWindowWidthAndHeight = () => {
@@ -29,10 +44,6 @@ export default class ResizableDraggableDialog extends React.Component {
       windowWidth,
       windowHeight
     };
-  };
-
-  onDragStop = (e, d) => {
-    this.setState({ x: Math.round(d.x), y: Math.round(d.y) });
   };
 
   render() {
@@ -50,8 +61,6 @@ export default class ResizableDraggableDialog extends React.Component {
         style={{ top: 0, left: 0, position: "fixed" }}
       >
         <Rnd
-          onDragStop={this.onDragStop}
-          position={{ x: this.state.x, y: this.state.y }}
           enableResizing={{
             bottomLeft: true,
             bottomRight: true,
@@ -64,6 +73,10 @@ export default class ResizableDraggableDialog extends React.Component {
           // minHeight={Math.min(defaultDialogHeight, 200) }
           bounds="window"
           default={{
+            x: Math.round(Math.max((windowWidth - defaultDialogWidth) / 2, 0)),
+            y: Math.round(
+              Math.max((windowHeight - defaultDialogHeight) / 2, 0)
+            ),
             width: Math.min(defaultDialogWidth, windowWidth),
             height: Math.min(defaultDialogHeight, windowHeight)
           }}
