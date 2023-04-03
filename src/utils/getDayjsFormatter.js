a476b1e1b35fca5afc19625e2d3adbd7149251fd
Taoh Green <taohgreen@gmail.com>
2023-04-03 19:47:55 +0200

    reset broken code

--- src/utils/getDayjsFormatter.js
+++ src/utils/getDayjsFormatter.js
@@ -1,16 +1,7 @@
 import dayjs from "dayjs";
 import LocalizedFormat from "dayjs/plugin/localizedFormat";
-import localeData from "dayjs/plugin/localeData";
 
-dayjs.extend(localeData);
 dayjs.extend(LocalizedFormat);
-const userLocale = Intl.DateTimeFormat().resolvedOptions().locale;
-
-if (userLocale) {
-  const localeToUse = userLocale.split("-")[0];
-  require(`dayjs/locale/${localeToUse}`);
-  dayjs.locale(localeToUse);
-}
 
 export default function getDayjsFormatter(format) {
   return {
