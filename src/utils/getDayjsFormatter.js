d96321729a6a35c822bb94784db9c1a13a371ca8
Taoh Green <taohgreen@gmail.com>
2023-04-03 19:54:39 +0200

    Load user locale to be used by dayjs formatter

--- src/utils/getDayjsFormatter.js
+++ src/utils/getDayjsFormatter.js
@@ -1,7 +1,16 @@
 import dayjs from "dayjs";
 import LocalizedFormat from "dayjs/plugin/localizedFormat";
+import localeData from "dayjs/plugin/localeData";
 
+dayjs.extend(localeData);
 dayjs.extend(LocalizedFormat);
+const userLocale = Intl.DateTimeFormat().resolvedOptions().locale;
+
+if (userLocale) {
+  const localeToUse = userLocale.split("-")[0];
+  require(`dayjs/locale/${localeToUse}.js`);
+  dayjs.locale(localeToUse);
+}
 
 export default function getDayjsFormatter(format) {
   return {
